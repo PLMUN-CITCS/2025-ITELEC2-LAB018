ad299ebb4a7792d50c52f8e43d20c2c44cc49d14
Joshua Tigno <tignojoshua@plmun.edu.ph>
2025-02-17 20:39:32 +0800

    Initial Project Files

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the Python program and test all input/output pairs in the tests folder
-python_script="switch_ternary_operator.py"
+python_script="nested_functions.py"
 
 declare -A results
 
